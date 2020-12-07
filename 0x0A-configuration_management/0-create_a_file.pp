309e9a6a431bb8dee9245157c05b76ee9ac8238a
Lilsherlock <potatogames15@gmail.com>
2020-12-07 13:38:11 -0500

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,7 +3,7 @@
 file { '/tmp/holberton':
   ensure  => file,
   path    => '/tmp/holbertonschool',
-  mode    => '0744'
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet'
