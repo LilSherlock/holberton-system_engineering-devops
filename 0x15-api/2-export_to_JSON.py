8e2ab3a74a4173692f0a77ceb7a8b5508d15c495
xd <xd@xds-MacBook-Pro.local>
2021-02-01 19:26:57 -0500

    update

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
-"""Exports data in the CSV format"""
+"""Exports data in the JSON format"""
 
-from sys import argv
-import requests
-import json
+if __name__ == "__main__":
 
-if __name__ == '__main__':
+    import json
+    import requests
+    import sys
 
-    user = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
-    user = user.json()
-    todo = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1]))
-    todo = todo.json()
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
 
-    xd = []
-    for x in todo:
+    todoUser = {}
+    taskList = []
 
-        todo_dict = {}
-        todo_dict["task"] = x.get("title")
-        todo_dict["completed"] = x.get('completed')
-        todo_dict["username"] = user.get('username')
-        xd.append(todo_dict)
+    for task in todos:
+        if task.get('userId') == int(userId):
+            taskDict = {"task": task.get('title'),
+                        "completed": task.get('completed'),
+                        "username": user.json().get('username')}
+            taskList.append(taskDict)
+    todoUser[userId] = taskList
 
-    json_dict = {}
-    json_dict[argv[1]] = xd
-
-    with open("{}.json".format(argv[1]), 'w') as json_file:
-        json.dump(json_dict, json_file)
+    filename = userId + '.json'
+    with open(filename, mode='w') as f:
+        json.dump(todoUser, f)
